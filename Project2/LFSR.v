1aff72da02d63900885e259bc29f8fe0f38a140a
Brandon Lewien <33644290+brandonlewien@users.noreply.github.com>
2018-04-28 08:20:37 -0600

    Create LFSR.v

---
+++ Project2/LFSR.v
@@ -0,0 +1,25 @@
+module LFSR(input clock, output reg [11:0] random);
+	initial begin
+		random = 12'b111111111111;
+	end
+	always @(posedge clock) begin
+		if (random == 0) begin
+			random <= 12'b111111111111;
+		end
+		else begin
+			random <= {random[10], 
+						  random[9],
+						  random[8],
+						  random[7],
+						  random[6],
+					  	  random[5],
+						  random[4],
+					  	  random[3],
+						  random[2],
+						  random[1],
+						  random[0],
+						  random[8] ^ random[3]
+			           };
+		end
+	end
+endmodule
